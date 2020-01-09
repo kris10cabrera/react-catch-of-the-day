6dbb2a9da086fb33b31a1fce7f244ced820be4f3
Kristen Cabrera <cabrera.kristen@gmail.com>
2020-01-08 21:14:11 -0500

    edit availability and price

--- src/components/Fish.js
+++ src/components/Fish.js
@@ -6,11 +6,14 @@ class Fish extends React.Component {
   render() {
     // es6 destructure!
     const {image, name, price, desc, status} = this.props.details;
+    const isAvailable = status === 'available';
     return (
       <li className="menu-fish">
         <img src={image} alt={name} />
-        <h3 className="fish-name">{name}</h3>
-        <span className="price">{formatPrice(price)}</span>
+        <h3 className="fish-name">{name}
+        <span className="price">{formatPrice(price)}</span></h3>
+        <p>{desc}</p>
+    <button disabled={!isAvailable}>{isAvailable ? 'Add To Order' : 'Sold Out'}</button>
       </li>
     )
   }
