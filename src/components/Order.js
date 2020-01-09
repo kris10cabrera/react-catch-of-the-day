6183256618da646aa43dee90ee70d16380004309
kris10cabrera <cabrera.kristen@gmail.com>
2020-01-09 12:02:45 -0500

    pass props to order component

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,13 +1,37 @@
 import React from 'react';
+import { formatPrice } from '../helpers';
 
 class Order extends React.Component {
   render() {
+    const orderIds = Object.keys(this.props.order);
+    // like a for loop or a Map, but instead of updated an external variable, it returns a specific subset. we're looking for a tally
+    const total = orderIds.reduce((prevTotal, key) => {
+      const fish = this.props.fishes[key];
+      const count = this.props.order[key];
+      const isAvailable = fish && fish.status == 'available';
+      if (isAvailable) {
+        return prevTotal + (count * fish.price);
+      }
+      return prevTotal;
+    }, 0); 
+    // have to start with 0 or a number when using reduce 
     return (
       <div className="order">
-        eexw
+        <div className="order-wrap">
+          <h2>Order</h2>
+          <ul>
+            { orderIds.map(key => <li>{key}</li>) }
+          </ul>
+          
+          <div className="total">
+            Total: <strong>{formatPrice(total)}</strong>
+          </div>
+        </div>
       </div>
     );
   }
 }
 
 export default Order;
+
+// on 10:30 of video 17
