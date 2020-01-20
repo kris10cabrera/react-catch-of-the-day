acf7baa854bcdb376c4e8dad983cd4509e2d8507
Kristen Cabrera <cabrera.kristen@gmail.com>
2020-01-20 16:15:14 -0500

    Update order to have finished text

--- src/components/Order.js
+++ src/components/Order.js
@@ -2,8 +2,23 @@ import React from 'react';
 import { formatPrice } from '../helpers';
 
 class Order extends React.Component {
-  renderOrder = (key) => {
-    return <li>{key}</li>;
+  renderOrder = key => {
+    const fish = this.props.fishes[key];
+    const count = this.props.order[key];
+    const isAvailable = fish.status === "available";
+    if (!isAvailable) {
+      return (
+        <li key={key}>
+          Sorry {fish ? fish.name : "fish"} is no longer available
+        </li>
+      );
+    }
+    return (
+      <li key={key}>
+        {count} lbs {fish.name}
+        {formatPrice(count * fish.price)}
+      </li>
+    );
   };
   render() {
     const orderIds = Object.keys(this.props.order);
@@ -11,21 +26,21 @@ class Order extends React.Component {
     const total = orderIds.reduce((prevTotal, key) => {
       const fish = this.props.fishes[key];
       const count = this.props.order[key];
-      const isAvailable = fish && fish.status == 'available';
+      const isAvailable = fish && fish.status === 'available';
       if (isAvailable) {
         return prevTotal + (count * fish.price);
       }
       return prevTotal;
-    }, 0); 
+    }, 0);
     // have to start with 0 or a number when using reduce 
     return (
       <div className="order">
         <div className="order-wrap">
           <h2>Order</h2>
-          <ul>
-            { orderIds.map(this.renderOrder)}
+          <ul className="order">
+            {orderIds.map(this.renderOrder)}
           </ul>
-          
+
           <div className="total">
             Total: <strong>{formatPrice(total)}</strong>
           </div>
